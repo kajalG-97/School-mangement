8d6aaa1774e3850624d3104f7c7787186d531537
KAJAL GANORKAR <kajalganorkar97@gmail.com>
2022-04-19 19:58:58 +0530

    project done

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,7 @@
 
 export const Home = () => {
     return (
-        <div style={{ backgroundColor:"#ebe7ff",height:"660px",marginTop:0}}>
+        <div style={{ backgroundColor:"#ebe7ff",height:"560px",marginTop:0}}>
             <br/>
             <h1>Welcome School Mangement Website</h1>
             <img src="https://image.shutterstock.com/image-vector/concept-new-team-member-welcome-260nw-1327428803.jpg" />
